b6736ee48070de690313b1f5ecc32e4d15621891
parmentelat <thierry.parmentelat@inria.fr>
2018-03-22 10:53:26 +0100

    a script to list user accounts together with, and sorted by, their creation date

---
+++ usage-statistics/list-users-by-creation-date.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env plcsh
+
+from __future__ import print_function
+
+import time
+
+def printable_time(epoch):
+    return time.strftime("%Y %m %d", time.localtime(epoch))
+
+ps = GetPersons()
+ps.sort(key = lambda p: p['date_created'])
+
+def list_accounts():
+    for p in ps:
+        print(printable_time(p['date_created']), "id={:03d}".format(p['person_id']), p['email'])
+
+import sys
+
+with open('ACCOUNTS', 'w') as sys.stdout:
+    list_accounts()
