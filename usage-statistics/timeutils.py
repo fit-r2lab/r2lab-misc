5350b5cbf799c5ec6fb54a91872deba41acd69ff
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2018-10-12 14:28:02 +0200

    tweaked for usage statistics as done in Oct. 2018

--- usage-statistics/timeutils.py
+++ usage-statistics/timeutils.py
@@ -22,12 +22,26 @@ def parse_date(input_string, end=False):
         An int-based timestamp,
     """
     parse_format = "%Y-%m-%d"
-    print('input_string', input_string)
+    # print('input_string', input_string)
     begin = time.mktime(time.strptime(input_string, parse_format))
     # add one day if required with end=True
     return begin if not end else begin + 24 * 3600
 
 
-def input_default(message, default):
-    prompt = f"{message} [{default}] : "
-    return input(prompt) or default
+ifrom = None
+iuntil = None
+
+def show_period():
+    global ifrom
+    if ifrom is None:
+        sfrom = input("Enter starting day yyyy-mm-dd : ")
+        ifrom = parse_date(sfrom)
+    print(f"period starts on {human_readable(ifrom)}")        
+    global iuntil
+    if iuntil is None:
+        suntil = input("Enter ending day yyyy-mm-dd : ")
+        iuntil = parse_date(suntil)
+    print(f"period starts on {human_readable(iuntil)}")
+    return ifrom, iuntil
+
+        
